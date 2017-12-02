1d255ed794dfefb0303d07dc7433bd3491fcf459
Valentin Popov <info@valentineus.link>
2017-12-02 16:02:08 +0400

    Build script

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# Author:       Valentin Popov
+# Email:        info@valentineus.link
+# Date:         2017-12-02
+# Usage:        /bin/sh build.sh
+# Description:  Build the final package for installation in Moodle.
+
+# Updating the Environment
+PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+export PATH="$PATH:/usr/local/scripts"
+
+# Build the package
+cd ..
+mv "./moodle-auth_token" "./auth_token"
+zip -9 -r "auth_token.zip" "auth_token" \
+        -x "auth_token/.git*"           \
+        -x "auth_token/.travis.yml"     \
+        -x "auth_token/build.sh"
+
+# End of work
+exit 0

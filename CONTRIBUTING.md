ac0b3e77420acebce04766d9fe4b20c2f344924c
Valentin Popov <info@valentineus.link>
2017-12-15 11:48:55 +0400

    The file 'CONTRIBUTING.md' was added

---
+++ CONTRIBUTING.md
@@ -0,0 +1,13 @@
+# Contributing
+
+:heart:
+We love pull requests from everyone.
+By participating in this project, you agree to abide by the
+[code of conduct](https://opensource.guide/how-to-contribute).
+
+Some things that will increase the chance that your pull request is accepted:
+* Write tests.
+* Follow our
+[style guide](https://docs.moodle.org/dev/Coding_style).
+* Write a
+[good commit message](http://tbaggery.com/2008/04/19/a-note-about-git-commit-messages.html).
--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Author:       Valentin Popov
 # Email:        info@valentineus.link
-# Date:         2017-12-02
+# Date:         2017-12-15
 # Usage:        /bin/sh build.sh
 # Description:  Build the final package for installation in Moodle.
 
@@ -12,9 +12,10 @@ export PATH="$PATH:/usr/local/scripts"
 # Build the package
 cd ..
 mv "./moodle-auth_token" "./auth_token"
-zip -9 -r "auth_token.zip" "auth_token" \
-        -x "auth_token/.git*"           \
-        -x "auth_token/.travis.yml"     \
+zip -9 -r "auth_token.zip" "auth_token"  \
+        -x "auth_token/.git*"            \
+        -x "auth_token/.travis.yml"      \
+        -x "auth_token/.CONTRIBUTING.md" \
         -x "auth_token/build.sh"
 
 # End of work
